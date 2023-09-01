9dcde69ba4dbacec217eabac84dbbdae25925cb5
shubhamanangare <138216515+shubhamanangare@users.noreply.github.com>
2023-09-01 22:43:18 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Pull') { 
             steps {
-                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/shubhamanangare/dockpipe-1.git']])
+             checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/shubhamanangare/dockpipe-1.git']])
             }
         }
        
