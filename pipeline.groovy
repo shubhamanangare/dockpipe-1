d220bb2682dddc6fb0149e1c23ee5c7691b8e015
shubhamanangare <138216515+shubhamanangare@users.noreply.github.com>
2023-09-01 22:38:04 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,37 +3,13 @@ pipeline {
     stages {
         stage('Pull') { 
             steps {
-                git 'https://github.com/Prajwal2023/dockpipe-1.git'
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/shubhamanangare/dockpipe-1.git']])
             }
         }
        
         stage('Build Docker Image') {
-            steps {
-                script {
-                    def dockerImage = docker.build(
-                        context: '.',
-                        dockerfile: '/home/ubuntu/Dockerfile',
-                        tags: "testapp:${env.BUILD_ID}"
-                    )
-                }
-            }
-        }
-
-        stage('Test') { 
-            steps {
-                echo 'Test successfully'
-            }
-        }
-
-        stage('Deploy on Same Instance') {
-            steps {
-                script {
-                    sh "docker stop testapp || true"  // Stop existing container if exists
-                    sh "docker rm testapp || true"    // Remove existing container if exists
-
-                    sh "docker run -d -p 8080:80 --testapp testapp:${env.BUILD_ID}"
-                }
-            }
+            sh docker build -t tomcat .
+            sh docker run -d -p 8082:8080 tomcat
         }
     }
 }
