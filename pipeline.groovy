892ec93849a3e219d0dab755f8ad63d762cd3cee
shubhamanangare <138216515+shubhamanangare@users.noreply.github.com>
2023-09-01 23:05:29 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,13 +3,19 @@ pipeline {
     stages {
         stage('Pull') { 
             steps {
-             checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/shubhamanangare/dockpipe-1.git']])
+                script {
+                    checkout([$class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        userRemoteConfigs: [[url: 'https://github.com/shubhamanangare/dockpipe-1.git']]])
+                }
             }
         }
        
         stage('Build Docker Image') {
-            sh docker build -t tomcat .
-            sh docker run -d -p 8082:8080 tomcat
+            steps {
+                sh 'docker build -t tomcat .'
+                sh 'docker run -d -p 8082:8080 tomcat'
+            }
         }
     }
 }
